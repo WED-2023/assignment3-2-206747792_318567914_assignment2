6b2308bae495513a2dcdf5b65d9b64986673a9c5
hadarkn <hadarknafo@gmail.com>
2025-05-26 12:43:12 +0300

    opening db files

---
+++ dist/db/connection.js
@@ -0,0 +1,12 @@
+// db/connection.js
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "",
+  database: process.env.DB_NAME || "recipes_db",
+});
+
+module.exports = pool;
